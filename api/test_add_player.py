ef20e91fe73a6fbf0c5cbb146ba041cb9ce15467
quentinf <quentinf@theodo.fr>
2018-04-13 23:18:33 +0200

    Add first unit test

---
+++ api/test_add_player.py
@@ -0,0 +1,9 @@
+import add_player
+from unittest.mock import patch
+
+@patch('add_player.load_state', lambda x: {})
+@patch('add_player.boto3.client', lambda x: 's3')
+@patch('add_player.dump_state')
+def test_run(dump_state_mock):
+    add_player.run({'queryStringParameters': {'player_name': 'toto'}}, None)
+    dump_state_mock.assert_called_with('s3', {'players': ['toto']})
